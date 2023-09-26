14a7e6cacbab7477adeeb004f3092c5a3e35d937
iblue08 <adutta@aeonixinnovations.com>
2023-09-26 17:12:33 +0530

    test

--- device_be_ts/dist/index.js
+++ device_be_ts/dist/index.js
@@ -9,12 +9,42 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = require("body-parser");
 const CounterRouter_1 = require("./routes/CounterRouter");
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT;
 const allowedOrigins = [
-    "http://localhost:5173"
+    "http://localhost:3001",
+    "http://192.168.1.4:3001",
+    "http://localhost:3000",
+    "https://docs-scanner-606c2.web.app"
 ];
+const server = http_1.default.createServer(app);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: allowedOrigins,
+        methods: ["GET", "POST"],
+    },
+});
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+    socket.on("join_room", (data) => {
+        socket.join(data);
+    });
+    socket.on("send_state", (data) => {
+        console.log("triggered send", data);
+        socket.to(data.room).emit("receive_state", data);
+    });
+    socket.on("reset_state", (data) => {
+        console.log("triggered send", data);
+        socket.to(data.room).emit("receive_reset_state", data);
+    });
+    socket.on("send_image_list", (data) => {
+        console.log("triggered image list send", data);
+        socket.to(data.room).emit("receive_image_list", data);
+    });
+});
+const port = process.env.PORT;
 const options = {
     origin: allowedOrigins,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
@@ -26,6 +56,6 @@ mongoose_1.default
     .connect(process.env.MONGO_URL)
     .then(() => console.log("  Database connected 📟 "))
     .catch((err) => console.log(err));
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
--- device_be_ts/src/index.ts
+++ device_be_ts/src/index.ts
@@ -4,13 +4,55 @@ import mongoose from "mongoose";
 import cors from "cors";
 import { json } from "body-parser";
 import { CameraRouter } from "./routes/CounterRouter";
+import http from "http";
+import {Server} from "socket.io";
+import fs from "fs";
 
 dotenv.config();
-const app: Express = express();
-const port = process.env.PORT!;
 
+const app: Express = express();
 const allowedOrigins = [
-  "http://localhost:5173"];
+  "http://localhost:3001",
+  "http://192.168.1.4:3001",
+  "http://localhost:3000",
+  "https://docs-scanner-606c2.web.app"
+];
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin:allowedOrigins ,
+    methods: ["GET", "POST"],
+  },
+});
+
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_state", (data) => {
+    console.log("triggered send",data);
+    socket.to(data.room).emit("receive_state", data);
+  });
+
+  socket.on("reset_state", (data) => {
+    console.log("triggered send",data);
+    socket.to(data.room).emit("receive_reset_state", data);
+  });
+
+  socket.on("send_image_list", (data) => {
+    console.log("triggered image list send",data);
+    socket.to(data.room).emit("receive_image_list", data);
+  });
+
+});
+
+const port = process.env.PORT!;
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
@@ -27,6 +69,6 @@ mongoose
   .catch((err) => console.log(err))
 
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
